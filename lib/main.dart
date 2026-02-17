66949683caf4b1724365bba0de13837c6ce21a38
fadhlurthoriq <muhammadfthoriq@gmail.com>
2026-02-18 05:40:57 +0700

    Modul 18 (Bikin dark mode)

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,19 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:project_chat/auth/auth_gate.dart';
 import 'package:project_chat/firebase_options.dart';
-import 'package:project_chat/themes/light_mode.dart';
+import 'package:project_chat/themes/theme_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform
   );
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget{
@@ -20,7 +25,7 @@ class MyApp extends StatelessWidget{
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: const AuthGate(),
-      theme: lightMode,
+      theme: Provider.of<ThemeProvider>(context).themeData ,
     );
   }
 }
